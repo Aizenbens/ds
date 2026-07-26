24768ef9687fac29de145c9605b214315cfcf163
Aizenbens <sheninozen@gmail.com>
2026-07-26 23:08:17 +0100

    Update App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,21 +1,6 @@
 import React from "react";
+import Scene from "./js/scene/Scene";
 
 export default function App() {
-  return (
-    <h1
-      style={{
-        color: "white",
-        background: "#111",
-        width: "100vw",
-        height: "100vh",
-        margin: 0,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontFamily: "Arial",
-      }}
-    >
-      FROM WHO?
-    </h1>
-  );
+  return <Scene />;
 }
