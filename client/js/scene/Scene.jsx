33574fcc1156b246deebadaf1d3f769781402512
Aizenbens <sheninozen@gmail.com>
2026-07-27 00:07:33 +0100

    Update Scene.jsx

--- client/js/scene/Scene.jsx
+++ client/js/scene/Scene.jsx
@@ -3,26 +3,28 @@ import { Canvas } from "@react-three/fiber";
 
 import Lights from "./Lights";
 import Sky from "./Sky";
+
+import Physics from "../physics/Physics";
 import World from "../world/World";
+import Player from "../player/Player";
 
 export default function Scene() {
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
+    <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas
         shadows
         camera={{
-          position: [0, 3, 8],
+          position: [0, 6, 12],
           fov: 75,
         }}
       >
         <Sky />
         <Lights />
-        <World />
+
+        <Physics>
+          <World />
+          <Player />
+        </Physics>
       </Canvas>
     </div>
   );
