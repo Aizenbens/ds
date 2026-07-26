d42586e7fc8ab1a48f00266df8e078b072387a40
Aizenbens <sheninozen@gmail.com>
2026-07-27 00:06:42 +0100

    Update Physics.jsx

--- client/js/ physics/Physics.jsx
+++ client/js/ physics/Physics.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+import { Physics as RapierPhysics } from "@react-three/rapier";
 
+export default function Physics({ children }) {
+  return (
+    <RapierPhysics gravity={[0, -9.81, 0]}>
+      {children}
+    </RapierPhysics>
+  );
+}
