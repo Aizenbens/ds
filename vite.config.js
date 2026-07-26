6e0668ec95446020e950c7ec436ec63fa36c9c23
Aizenbens <sheninozen@gmail.com>
2026-07-26 23:37:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "client",
-  plugins: [react],
+  plugins: [react()],
   build: {
     outDir: "../dist",
     emptyOutDir: true,
