9b918e2dc1eb064f9812641f9b6356dd990360b6
Aizenbens <sheninozen@gmail.com>
2026-07-26 22:28:42 +0100

    Create App.jsx

---
+++ client/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <h1
+      style={{
+        color: "white",
+        background: "#111",
+        width: "100vw",
+        height: "100vh",
+        margin: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "Arial",
+      }}
+    >
+      KRUNK WAR
+    </h1>
+  );
+}
