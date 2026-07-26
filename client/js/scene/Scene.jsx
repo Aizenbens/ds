d3c090cdeefae475fdf228bbf6333c8e6536a47c
Aizenbens <sheninozen@gmail.com>
2026-07-26 23:38:35 +0100

    Update Scene.jsx

--- client/js/scene/Scene.jsx
+++ client/js/scene/Scene.jsx
@@ -1,20 +1,22 @@
+import React from "react";
 import { Canvas } from "@react-three/fiber";
-
-function Box() {
-  return (
-    <mesh>
-      <boxGeometry />
-      <meshNormalMaterial />
-    </mesh>
-  );
-}
 
 export default function Scene() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas>
-        <ambientLight />
-        <Box />
+      <Canvas camera={{ position: [0, 2, 6], fov: 75 }}>
+        <ambientLight intensity={1} />
+        <directionalLight position={[5, 10, 5]} intensity={2} />
+
+        <mesh rotation={[-Math.PI / 2, 0, 0]}>
+          <planeGeometry args={[100, 100]} />
+          <meshStandardMaterial color="#4caf50" />
+        </mesh>
+
+        <mesh position={[0, 1, 0]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="orange" />
+        </mesh>
       </Canvas>
     </div>
   );
