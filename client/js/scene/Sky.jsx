8f3d9d4c92d2ea75e8cbc91424dc1559d5acb4db
Aizenbens <sheninozen@gmail.com>
2026-07-27 00:00:17 +0100

    Update Sky.jsx

--- client/js/scene/Sky.jsx
+++ client/js/scene/Sky.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+import { Sky as DreiSky } from "@react-three/drei";
 
+export default function Sky() {
+  return (
+    <DreiSky
+      distance={450000}
+      sunPosition={[10, 20, 10]}
+      inclination={0.5}
+      azimuth={0.25}
+    />
+  );
+}
