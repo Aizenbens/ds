957afcae4cab29f5608feec6581d72fa5ec7b270
Aizenbens <sheninozen@gmail.com>
2026-07-27 00:03:38 +0100

    Update World.jsx

--- client/js/world/World.jsx
+++ client/js/world/World.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+import Ground from "./Ground";
 
+export default function World() {
+  return (
+    <>
+      <Ground />
+
+      {/* مكعب تجريبي */}
+      <mesh position={[0, 1, 0]} castShadow>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+    </>
+  );
+}
