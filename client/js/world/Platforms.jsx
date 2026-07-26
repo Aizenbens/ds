c9239339e3322349c64d0ca9d135b78ca092fc2f
Aizenbens <sheninozen@gmail.com>
2026-07-27 00:04:14 +0100

    Update Platforms.jsx

--- client/js/world/Platforms.jsx
+++ client/js/world/Platforms.jsx
@@ -1,1 +1,27 @@
+import React from "react";
 
+const platforms = [
+  { position: [0, 0.5, 0], size: [4, 1, 4] },
+  { position: [6, 1.5, -2], size: [3, 1, 3] },
+  { position: [12, 3, -5], size: [3, 1, 3] },
+  { position: [18, 5, -8], size: [3, 1, 3] },
+  { position: [25, 7, -12], size: [4, 1, 4] },
+];
+
+export default function Platforms() {
+  return (
+    <>
+      {platforms.map((platform, index) => (
+        <mesh
+          key={index}
+          position={platform.position}
+          castShadow
+          receiveShadow
+        >
+          <boxGeometry args={platform.size} />
+          <meshStandardMaterial color="#808080" />
+        </mesh>
+      ))}
+    </>
+  );
+}
