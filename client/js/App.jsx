8b0a44df52f1993e521cc4c5db5363cc8fd9f88b
Aizenbens <sheninozen@gmail.com>
2026-07-26 22:48:06 +0100

    Update App.jsx

--- client/js/App.jsx
+++ client/js/App.jsx
@@ -1,1 +1,22 @@
+import React from "react";
 
+export default function App() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        margin: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#111",
+        color: "#fff",
+        fontSize: "48px",
+        fontFamily: "Arial",
+      }}
+    >
+      FROM WHO?
+    </div>
+  );
+}
