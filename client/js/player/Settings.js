e9ab13a29f3d7986778b057b60d476f35ea834da
Aizenbens <sheninozen@gmail.com>
2026-07-27 13:11:46 +0100

    Update Settings.js

--- client/js/player/Settings.js
+++ client/js/player/Settings.js
@@ -1,22 +1,27 @@
 const Settings = {
+  // الحركة
   walkSpeed: 5,
   sprintSpeed: 8,
 
+  // الفيزياء
+  acceleration: 25,
+  deceleration: 18,
   jumpForce: 8,
-
   gravity: -9.81,
 
+  // الطاقة
   stamina: 100,
   staminaDrain: 25,
   staminaRecovery: 20,
 
+  // الكاميرا
   mouseSensitivity: 0.002,
-
-  headBobSpeed: 10,
-  headBobAmount: 0.05,
-
   cameraFov: 75,
   sprintFov: 82,
+
+  // Head Bob (سنستخدمه لاحقًا)
+  headBobSpeed: 10,
+  headBobAmount: 0.05,
 };
 
 export default Settings;
