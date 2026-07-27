be4b0af176605ff994f84049f49baf35d7e825f8
Aizenbens <sheninozen@gmail.com>
2026-07-27 13:02:50 +0100

    Update HUD.jsx

--- client/js/ui/HUD.jsx
+++ client/js/ui/HUD.jsx
@@ -7,14 +7,20 @@ export default function HUD() {
         position: "fixed",
         top: "20px",
         left: "20px",
-        background: "red",
-        color: "white",
-        padding: "20px",
-        fontSize: "30px",
+        width: "300px",
+        height: "30px",
+        background: "#222",
+        border: "2px solid white",
         zIndex: 999999,
       }}
     >
-      HUD WORKING
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          background: "lime",
+        }}
+      />
     </div>
   );
 }
