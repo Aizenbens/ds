bb48d7d1b3dc8545d338da61551c36fdca1f628d
Aizenbens <sheninozen@gmail.com>
2026-07-27 13:16:18 +0100

    Create CameraController.jsx

---
+++ client/js/player/CameraController.jsx
@@ -0,0 +1,17 @@
+import { useFrame, useThree } from "@react-three/fiber";
+import Settings from "./Settings.js";
+import keys from "../input/Input.js";
+
+export default function CameraController() {
+  const { camera } = useThree();
+
+  useFrame((_, delta) => {
+    const targetFov =
+      keys["ShiftLeft"] ? Settings.sprintFov : Settings.cameraFov;
+
+    camera.fov += (targetFov - camera.fov) * delta * 8;
+    camera.updateProjectionMatrix();
+  });
+
+  return null;
+}
