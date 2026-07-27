ea8a727e1c397424545396c7dd00d7285063174f
Aizenbens <sheninozen@gmail.com>
2026-07-27 12:51:13 +0100

    Update Player.jsx

--- client/js/player/Player.jsx
+++ client/js/player/Player.jsx
@@ -4,11 +4,16 @@ import { RigidBody, CapsuleCollider } from "@react-three/rapier";
 import Camera from "./Camera.jsx";
 import Controls from "./Controls.jsx";
 import Movement from "./Movement.jsx";
+import Jump from "./Jump.jsx";
 
 export default function Player() {
   const body = useRef();
 
+  // نظام الحركة
   Movement(body);
+
+  // نظام القفز
+  Jump(body);
 
   return (
     <RigidBody
@@ -17,12 +22,21 @@ export default function Player() {
       type="dynamic"
       position={[0, 3, 0]}
       enabledRotations={[false, false, false]}
+      mass={1}
+      friction={1}
+      restitution={0}
+      canSleep={false}
     >
+      {/* Collider اللاعب */}
       <CapsuleCollider args={[0.5, 0.4]} />
 
+      {/* كاميرا اللاعب */}
       <Camera />
+
+      {/* تحريك الماوس */}
       <Controls />
 
+      {/* جسم مؤقت للتجربة */}
       <mesh castShadow>
         <capsuleGeometry args={[0.4, 1, 8, 16]} />
         <meshStandardMaterial color="dodgerblue" />
