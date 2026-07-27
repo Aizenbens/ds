e380863467914cf5541259d4eadf4a532b406130
Aizenbens <sheninozen@gmail.com>
2026-07-27 12:45:38 +0100

    Update Movement.js

--- client/js/player/Movement.js
+++ client/js/player/Movement.js
@@ -1,1 +1,29 @@
+import { useFrame } from "@react-three/fiber";
+import keys from "../input/Input.js";
 
+export default function Movement(body) {
+  useFrame(() => {
+    if (!body.current) return;
+
+    const velocity = body.current.linvel();
+
+    let x = 0;
+    let z = 0;
+
+    const speed = keys["ShiftLeft"] ? 8 : 5;
+
+    if (keys["KeyW"]) z -= speed;
+    if (keys["KeyS"]) z += speed;
+    if (keys["KeyA"]) x -= speed;
+    if (keys["KeyD"]) x += speed;
+
+    body.current.setLinvel(
+      {
+        x,
+        y: velocity.y,
+        z,
+      },
+      true
+    );
+  });
+}
