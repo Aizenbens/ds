78dc8ff4d11b66ae100549eab248b484fea1a707
Aizenbens <sheninozen@gmail.com>
2026-07-27 12:33:38 +0100

    Update Camera.jsx

--- client/js/player/Camera.jsx
+++ client/js/player/Camera.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+import { PerspectiveCamera } from "@react-three/drei";
 
+export default function Camera() {
+  return (
+    <PerspectiveCamera
+      makeDefault
+      position={[0, 0.6, 0]}
+      fov={75}
+    />
+  );
+}
