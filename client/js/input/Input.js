7f09667ef3846d43810d9d387f413fe01f6da6d8
Aizenbens <sheninozen@gmail.com>
2026-07-27 12:43:05 +0100

    Update Input.js

--- client/js/input/Input.js
+++ client/js/input/Input.js
@@ -1,1 +1,11 @@
+const keys = {};
 
+window.addEventListener("keydown", (event) => {
+  keys[event.code] = true;
+});
+
+window.addEventListener("keyup", (event) => {
+  keys[event.code] = false;
+});
+
+export default keys;
