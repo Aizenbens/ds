622eef21e360d32b39b1a4b59bd11cf1e5f49dd0
Aizenbens <sheninozen@gmail.com>
2026-07-27 12:43:31 +0100

    Update Player.jsx

--- client/js/player/Player.jsx
+++ client/js/player/Player.jsx
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 import { RigidBody, CapsuleCollider } from "@react-three/rapier";
+import * as THREE from "three";
 
 import Camera from "./Camera.jsx";
 import Controls from "./Controls.jsx";
+import keys from "../input/Input.js";
 
 export default function Player() {
+  const body = useRef();
+
+  useFrame(() => {
+    if (!body.current) return;
+
+    const velocity = body.current.linvel();
+
+    let x = 0;
+    let z = 0;
+
+    const speed = keys["ShiftLeft"] ? 8 : 5;
+
+    if (keys["KeyW"]) z -= speed;
+    if (keys["KeyS"]) z += speed;
+    if (keys["KeyA"]) x -= speed;
+    if (keys["KeyD"]) x += speed;
+
+    body.current.setLinvel(
+      {
+        x,
+        y: velocity.y,
+        z,
+      },
+      true
+    );
+  });
+
   return (
     <RigidBody
+      ref={body}
       colliders={false}
       type="dynamic"
       position={[0, 3, 0]}
       enabledRotations={[false, false, false]}
-      mass={1}
     >
-      {/* Collider اللاعب */}
       <CapsuleCollider args={[0.5, 0.4]} />
 
-      {/* كاميرا اللاعب */}
       <Camera />
-
-      {/* التحكم بالماوس */}
       <Controls />
 
-      {/* جسم اللاعب (مؤقتًا) */}
       <mesh castShadow>
         <capsuleGeometry args={[0.4, 1, 8, 16]} />
         <meshStandardMaterial color="dodgerblue" />
