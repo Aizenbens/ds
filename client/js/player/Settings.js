a59a7c90cb64450ea2497ea5b9c9f7c41fe18f5f
Aizenbens <sheninozen@gmail.com>
2026-07-27 12:49:36 +0100

    Update Settings.js

--- client/js/player/Settings.js
+++ client/js/player/Settings.js
@@ -1,1 +1,22 @@
+const Settings = {
+  walkSpeed: 5,
+  sprintSpeed: 8,
 
+  jumpForce: 8,
+
+  gravity: -9.81,
+
+  stamina: 100,
+  staminaDrain: 25,
+  staminaRecovery: 20,
+
+  mouseSensitivity: 0.002,
+
+  headBobSpeed: 10,
+  headBobAmount: 0.05,
+
+  cameraFov: 75,
+  sprintFov: 82,
+};
+
+export default Settings;
