dd902ea3c7a872c3a8258e7ea2f7629afcecc88f
Aizenbens <sheninozen@gmail.com>
2026-07-27 13:01:57 +0100

    Update App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,18 @@
 import React from "react";
-import Scene from "./js/scene/Scene.jsx";
 
 export default function App() {
-  return <Scene />;
+  return (
+    <h1
+      style={{
+        color: "red",
+        fontSize: "60px",
+        position: "fixed",
+        top: "50px",
+        left: "50px",
+        zIndex: 999999,
+      }}
+    >
+      TEST APP
+    </h1>
+  );
 }
