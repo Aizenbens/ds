3cbb04783fb96e06120348b779eb2d81cec23b02
Aizenbens <sheninozen@gmail.com>
2026-07-27 13:17:00 +0100

    Update Camera.jsx

--- client/js/player/Camera.jsx
+++ client/js/player/Camera.jsx
@@ -1,17 +1,15 @@
-import { useFrame, useThree } from "@react-three/fiber";
-import Settings from "./Settings.js";
-import keys from "../input/Input.js";
+import { useThree, useFrame } from "@react-three/fiber";
+import { useEffect } from "react";
 
-export default function CameraController() {
+export default function Camera() {
   const { camera } = useThree();
 
-  useFrame((_, delta) => {
-    const targetFov = keys["ShiftLeft"]
-      ? Settings.sprintFov
-      : Settings.cameraFov;
+  useEffect(() => {
+    camera.position.set(0, 0.8, 0);
+  }, [camera]);
 
-    camera.fov += (targetFov - camera.fov) * delta * 8;
-    camera.updateProjectionMatrix();
+  useFrame(() => {
+    // سيتم إضافة Head Bob لاحقًا هنا
   });
 
   return null;
