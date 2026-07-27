c65377085c0d0e00469000b6390d03d75f85b1b5
Aizenbens <sheninozen@gmail.com>
2026-07-27 13:13:48 +0100

    Update Camera.jsx

--- client/js/player/Camera.jsx
+++ client/js/player/Camera.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import { PerspectiveCamera } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import Settings from "./Settings.js";
+import keys from "../input/Input.js";
 
-export default function Camera() {
-  return (
-    <PerspectiveCamera
-      makeDefault
-      position={[0, 0.6, 0]}
-      fov={75}
-    />
-  );
+export default function CameraController() {
+  const { camera } = useThree();
+
+  useFrame((_, delta) => {
+    const targetFov = keys["ShiftLeft"]
+      ? Settings.sprintFov
+      : Settings.cameraFov;
+
+    camera.fov += (targetFov - camera.fov) * delta * 8;
+    camera.updateProjectionMatrix();
+  });
+
+  return null;
 }
