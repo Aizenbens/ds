5afeffb90fcb57e2276f4df72f15f9ae2472c361
Aizenbens <sheninozen@gmail.com>
2026-07-27 12:50:48 +0100

    Update Jump.jsx

--- client/js/player/Jump.jsx
+++ client/js/player/Jump.jsx
@@ -1,1 +1,34 @@
+import { useFrame } from "@react-three/fiber";
+import keys from "../input/Input.js";
+import Settings from "./Settings.js";
 
+let canJump = true;
+
+export default function Jump(body) {
+  useFrame(() => {
+    if (!body.current) return;
+
+    const velocity = body.current.linvel();
+    const position = body.current.translation();
+
+    // يعتبر اللاعب على الأرض عندما يكون قريبًا منها
+    const grounded = position.y <= 1.55;
+
+    if (grounded) {
+      canJump = true;
+    }
+
+    if (keys["Space"] && grounded && canJump) {
+      body.current.setLinvel(
+        {
+          x: velocity.x,
+          y: Settings.jumpForce,
+          z: velocity.z,
+        },
+        true
+      );
+
+      canJump = false;
+    }
+  });
+}
