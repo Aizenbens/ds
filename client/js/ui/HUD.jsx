aece9ec4d1f70b0a1211cac6cd18fcf81e04ea3f
Aizenbens <sheninozen@gmail.com>
2026-07-27 12:56:37 +0100

    Update HUD.jsx

--- client/js/ui/HUD.jsx
+++ client/js/ui/HUD.jsx
@@ -1,1 +1,22 @@
+import React, { useState, useEffect } from "react";
+import StaminaBar from "./StaminaBar.jsx";
 
+export default function HUD() {
+  const [stamina, setStamina] = useState(100);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (window.playerStamina !== undefined) {
+        setStamina(window.playerStamina);
+      }
+    }, 16);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <StaminaBar value={stamina} />
+    </>
+  );
+}
