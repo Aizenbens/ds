b6280fc2f7d6ad1a20ba59785dc7d527a01c6323
Aizenbens <sheninozen@gmail.com>
2026-07-28 12:38:35 +0100

    Update Scene.jsx

--- client/js/scene/Scene.jsx
+++ client/js/scene/Scene.jsx
@@ -16,21 +16,24 @@ export default function Scene() {
         height: "100vh",
       }}
     >
-     <Canvas
-  shadows
-  camera={{
-    position: [0, 3, 8],
-    fov: 75,
-  }}
->
-  <axesHelper args={[5]} />
-  <gridHelper args={[20, 20]} />
+      <Canvas
+        shadows
+        camera={{
+          position: [0, 3, 8],
+          fov: 75,
+        }}
+      >
+        <axesHelper args={[5]} />
+        <gridHelper args={[20, 20]} />
 
-  <Sky />
-  <Lights />
+        <Sky />
+        <Lights />
 
-  <Physics>
-    <World />
-    <Player />
-  </Physics>
-</Canvas>
+        <Physics>
+          <World />
+          <Player />
+        </Physics>
+      </Canvas>
+    </div>
+  );
+}
