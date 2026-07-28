7b5ebd2c1e15f1f785375bcd7595a53d14440e3f
Aizenbens <sheninozen@gmail.com>
2026-07-28 12:51:58 +0100

    Update Scene.jsx

--- client/js/scene/Scene.jsx
+++ client/js/scene/Scene.jsx
@@ -3,17 +3,23 @@ import { Canvas } from "@react-three/fiber";
 
 export default function Scene() {
   return (
-    <div style={{ width: "100vw", height: "100vh", background: "black" }}>
-      <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
-        <color attach="background" args={["#202020"]} />
+    <Canvas
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: "black",
+      }}
+      camera={{
+        position: [0, 0, 5],
+        fov: 75,
+      }}
+    >
+      <ambientLight intensity={2} />
 
-        <ambientLight intensity={2} />
-
-        <mesh>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshNormalMaterial />
-        </mesh>
-      </Canvas>
-    </div>
+      <mesh>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshNormalMaterial />
+      </mesh>
+    </Canvas>
   );
 }
