894c169904f7e160d23177c726952afa23332136
Aizenbens <sheninozen@gmail.com>
2026-07-28 12:50:22 +0100

    Update Scene.jsx

--- client/js/scene/Scene.jsx
+++ client/js/scene/Scene.jsx
@@ -1,27 +1,17 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 
-import Lights from "./Lights.jsx";
-import Sky from "./Sky.jsx";
-
 export default function Scene() {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas
-        camera={{
-          position: [0, 5, 10],
-          fov: 75,
-        }}
-      >
-        <Sky />
-        <Lights />
+    <div style={{ width: "100vw", height: "100vh", background: "black" }}>
+      <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
+        <color attach="background" args={["#202020"]} />
 
-        <gridHelper args={[20, 20]} />
-        <axesHelper args={[5]} />
+        <ambientLight intensity={2} />
 
-        <mesh position={[0, 1, 0]}>
+        <mesh>
           <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial color="red" />
+          <meshNormalMaterial />
         </mesh>
       </Canvas>
     </div>
