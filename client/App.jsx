22f996e285f3626ff8dbd064be4f41d090517a5c
Aizenbens <sheninozen@gmail.com>
2026-07-28 12:48:35 +0100

    Update App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-import { GameProvider } from "./js/core/GameManager.jsx";
-
 import Scene from "./js/scene/Scene.jsx";
 import HUD from "./js/ui/HUD.jsx";
 
 export default function App() {
   return (
-    <GameProvider>
+    <>
       <Scene />
       <HUD />
-    </GameProvider>
+    </>
   );
 }
